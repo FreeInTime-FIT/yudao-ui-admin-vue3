c8aa96e72b83c4730585c535b9c9dcd55e4b34d1
yinshi <yinshi@cnabcpm.com>
2024-06-26 17:13:47 +0800

    fix: 修改展示问题

--- src/views/screen/components/CardHeader.vue
+++ src/views/screen/components/CardHeader.vue
@@ -5,29 +5,38 @@ defineOptions({
 })
 const { title } = defineProps<{
   title: string;
+  size?: 'small' | 'default';
 }>()
 </script>
 
 <template>
-  <header class="card-header">
-    <div class="card-header_arrow_left">
-      <div class="card-header_arrow_left_first"></div>
-      <div class="card-header_arrow_left_second"></div>
+  <header class="screen-card-header">
+    <div class="screen-card-header_arrow_left">
+      <div class="screen-card-header_arrow_left_first"></div>
+      <div class="screen-card-header_arrow_left_second"></div>
     </div>
     <h3>{{title}}</h3>
-    <div class="card-header_arrow_right">
-      <div class="card-header_arrow_right_first"></div>
-      <div class="card-header_arrow_right_second"></div>
+    <div class="screen-card-header_arrow_right">
+      <div class="screen-card-header_arrow_right_first"></div>
+      <div class="screen-card-header_arrow_right_second"></div>
     </div>
   </header>
 </template>
 
 <style scoped lang="scss">
-  .card-header{
+  $arrowFirstWidth: 2px;
+  .screen-card-header{
     position: relative;
     background: linear-gradient(to right, #111c38, rgb(24 50 117) 50% , #111c38);
     padding: 4px 12px;
     margin-block: 12px;
+
+    --arrow-first-top: 1px;
+    --arrow-first-width: 2px;
+    --arrow-first-height: 8px;
+    --arrow-second-top: -3px;
+    --arrow-second-width: 3px;
+    --arrow-second-height: 3px;
     h3{
       text-shadow: 0 0 5px #000;
       padding: 3px 0;
@@ -36,9 +45,11 @@ const { title } = defineProps<{
       margin: 0;
     }
     &_arrow_left{
+
+
       &_first{
         position: absolute;
-        width: 4px;
+        width: var(--arrow-first-width);
         top: 0;
         bottom: 0;
         left: 0px;
@@ -47,10 +58,10 @@ const { title } = defineProps<{
         &:before{
           content: '';
           position: absolute;
-          width: 4px;
-          top: 3px;
+          width: var(--arrow-first-width);
+          top: var(--arrow-first-top);
           background-color: #2d68c5;
-          height: 16px;
+          height: var(--arrow-first-height);
           transform-origin: 2px 0;
           transform: rotate(-100deg) skew(30deg, 0deg);
           left: -1px;
@@ -58,10 +69,10 @@ const { title } = defineProps<{
         &:after{
           content: '';
           position: absolute;
-          width: 4px;
-          bottom: 4px;
+          width:  var(--arrow-first-width);
+          bottom: var(--arrow-first-top);
           background-color: #2d68c5;
-          height: 16px;
+          height: var(--arrow-first-height);
           transform-origin: 2px 100%;
           transform: rotate(100deg) skew(-30deg, 0);
           left: -2px;
@@ -69,13 +80,40 @@ const { title } = defineProps<{
       }
       &_second{
         position: absolute;
+        width: var(--arrow-second-width);
+        top: 4px;
+        bottom: 4px;
+        left: 10px;
+        background-color: #72f6fa;
+        &:before{
+          content: '';
+          position: absolute;
+          width: var(--arrow-second-width);
+          top: var(--arrow-second-top);
+          height: var(--arrow-second-height);
 
+          background: linear-gradient(to top right, #72f6fa, transparent);
+          transform-origin: 2px 100%;
+          transform: skew(-45deg, 0);
+          left: 0px;
+        }
+        &:after{
+          content: '';
+          position: absolute;
+          width: var(--arrow-second-width);
+          bottom: var(--arrow-second-top);
+          height: var(--arrow-second-height);
+          background: linear-gradient(to left bottom, #72f6fa, transparent);
+          transform-origin: 2px 0;
+          transform: skew(45deg, 0deg);
+          left: 0;
+        }
       }
     }
     &_arrow_right{
       &_first{
         position: absolute;
-        width: 4px;
+        width:  var(--arrow-first-width);
         top: 0;
         bottom: 0;
         right: 0px;
@@ -84,24 +122,54 @@ const { title } = defineProps<{
         &:before{
           content: '';
           position: absolute;
-          width: 4px;
-          top: 3px;
+          width: var(--arrow-first-width);
+          top: var(--arrow-first-top);
           background-color: #2d68c5;
-          height: 16px;
+          height: var(--arrow-first-height);
           transform-origin: 2px 0;
           transform: rotate(100deg) skew(-30deg, 0deg);
-          left: 2px;
+          left: 0;
         }
         &:after{
           content: '';
           position: absolute;
-          width: 4px;
-          bottom: 4px;
+          width:  var(--arrow-first-width);
+          bottom: var(--arrow-first-top);
           background-color: #2d68c5;
-          height: 16px;
+          height: var(--arrow-first-height);
           transform-origin: 2px 100%;
           transform: rotate(-100deg) skew(30deg, 0);
-          left: 2px;
+          left: 0;
+        }
+      }
+      &_second{
+        position: absolute;
+        width: var(--arrow-second-width);
+        top: 4px;
+        bottom: 4px;
+        right: 10px;
+        background-color: #72f6fa;
+        &:before{
+          content: '';
+          position: absolute;
+          width: var(--arrow-second-width);
+          top: var(--arrow-second-top);
+          height: var(--arrow-second-height);
+          background: linear-gradient(to right top, #72f6fa, transparent);
+          transform-origin: 2px 0;
+          transform: skew(45deg, 0deg);
+          left: -3px;
+        }
+        &:after{
+          content: '';
+          position: absolute;
+          width: var(--arrow-second-width);
+          bottom: var(--arrow-second-top);
+          height: var(--arrow-second-height);
+          background: linear-gradient(to bottom left, #72f6fa, transparent);
+          transform-origin: 2px 100%;
+          transform: skew(-45deg, 0);
+          left: -3px;
         }
       }
     }
