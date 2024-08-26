4f06ea6df5950dc67fa631165266d82e9af26f01
yinshi <yinshi@cnabcpm.com>
2024-08-26 20:52:11 +0800

    fix: 修改电价展示

--- src/views/iot/hourlyprices/HourlyPricesForm.vue
+++ src/views/iot/hourlyprices/HourlyPricesForm.vue
@@ -4,98 +4,100 @@
       ref="formRef"
       :model="formData"
       :rules="formRules"
-      label-width="100px"
+      label-width="60px"
       v-loading="formLoading"
     >
-      <el-form-item label="年月" prop="month">
-        <el-date-picker
-          v-model="formData.month"
-          value-format="YYYY-MM"
-          placeholder="选择日期"
-        />
-      </el-form-item>
-      <el-form-item label="项目" prop="projectId">
-        <el-select v-model="formData.projectId" placeholder="请输入项目编号">
-          <el-option
-            v-for="item in projectList"
-            :key="item.id"
-            :value="item.id"
-            :label="item.name"
+      <div class="price-form">
+        <el-form-item label="年月" prop="month">
+          <el-date-picker
+            v-model="formData.month"
+            value-format="YYYY-MM"
+            placeholder="选择日期"
           />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="1" prop="price01">
-        <el-input v-model="formData.price01" placeholder="请输入第1小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="2" prop="price02">
-        <el-input v-model="formData.price02" placeholder="请输入第2小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="3" prop="price03">
-        <el-input v-model="formData.price03" placeholder="请输入第3小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="4" prop="price04">
-        <el-input v-model="formData.price04" placeholder="请输入第4小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="5" prop="price05">
-        <el-input v-model="formData.price05" placeholder="请输入第5小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="6" prop="price06">
-        <el-input v-model="formData.price06" placeholder="请输入第6小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="7" prop="price07">
-        <el-input v-model="formData.price07" placeholder="请输入第7小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="8" prop="price08">
-        <el-input v-model="formData.price08" placeholder="请输入第8小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="9" prop="price09">
-        <el-input v-model="formData.price09" placeholder="请输入第9小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="10" prop="price10">
-        <el-input v-model="formData.price10" placeholder="请输入第10小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="11" prop="price11">
-        <el-input v-model="formData.price11" placeholder="请输入第11小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="12" prop="price12">
-        <el-input v-model="formData.price12" placeholder="请输入第12小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="13" prop="price13">
-        <el-input v-model="formData.price13" placeholder="请输入第13小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="14" prop="price14">
-        <el-input v-model="formData.price14" placeholder="请输入第14小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="15" prop="price15">
-        <el-input v-model="formData.price15" placeholder="请输入第15小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="16" prop="price16">
-        <el-input v-model="formData.price16" placeholder="请输入第16小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="17" prop="price17">
-        <el-input v-model="formData.price17" placeholder="请输入第17小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="18" prop="price18">
-        <el-input v-model="formData.price18" placeholder="请输入第18小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="19" prop="price19">
-        <el-input v-model="formData.price19" placeholder="请输入第19小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="20" prop="price20">
-        <el-input v-model="formData.price20" placeholder="请输入第20小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="21" prop="price21">
-        <el-input v-model="formData.price21" placeholder="请输入第21小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="22" prop="price22">
-        <el-input v-model="formData.price22" placeholder="请输入第22小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="23" prop="price23">
-        <el-input v-model="formData.price23" placeholder="请输入第23小时的价格" type="number"/>
-      </el-form-item>
-      <el-form-item label="24" prop="price24">
-        <el-input v-model="formData.price24" placeholder="请输入第24小时的价格" type="number"/>
-      </el-form-item>
+        </el-form-item>
+        <el-form-item label="项目" prop="projectId">
+          <el-select v-model="formData.projectId" placeholder="请输入项目编号">
+            <el-option
+              v-for="item in projectList"
+              :key="item.id"
+              :value="item.id"
+              :label="item.name"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="1" prop="price01">
+          <el-input v-model="formData.price01" placeholder="请输入第1小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="2" prop="price02">
+          <el-input v-model="formData.price02" placeholder="请输入第2小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="3" prop="price03">
+          <el-input v-model="formData.price03" placeholder="请输入第3小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="4" prop="price04">
+          <el-input v-model="formData.price04" placeholder="请输入第4小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="5" prop="price05">
+          <el-input v-model="formData.price05" placeholder="请输入第5小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="6" prop="price06">
+          <el-input v-model="formData.price06" placeholder="请输入第6小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="7" prop="price07">
+          <el-input v-model="formData.price07" placeholder="请输入第7小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="8" prop="price08">
+          <el-input v-model="formData.price08" placeholder="请输入第8小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="9" prop="price09">
+          <el-input v-model="formData.price09" placeholder="请输入第9小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="10" prop="price10">
+          <el-input v-model="formData.price10" placeholder="请输入第10小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="11" prop="price11">
+          <el-input v-model="formData.price11" placeholder="请输入第11小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="12" prop="price12">
+          <el-input v-model="formData.price12" placeholder="请输入第12小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="13" prop="price13">
+          <el-input v-model="formData.price13" placeholder="请输入第13小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="14" prop="price14">
+          <el-input v-model="formData.price14" placeholder="请输入第14小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="15" prop="price15">
+          <el-input v-model="formData.price15" placeholder="请输入第15小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="16" prop="price16">
+          <el-input v-model="formData.price16" placeholder="请输入第16小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="17" prop="price17">
+          <el-input v-model="formData.price17" placeholder="请输入第17小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="18" prop="price18">
+          <el-input v-model="formData.price18" placeholder="请输入第18小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="19" prop="price19">
+          <el-input v-model="formData.price19" placeholder="请输入第19小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="20" prop="price20">
+          <el-input v-model="formData.price20" placeholder="请输入第20小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="21" prop="price21">
+          <el-input v-model="formData.price21" placeholder="请输入第21小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="22" prop="price22">
+          <el-input v-model="formData.price22" placeholder="请输入第22小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="23" prop="price23">
+          <el-input v-model="formData.price23" placeholder="请输入第23小时的价格" type="number"/>
+        </el-form-item>
+        <el-form-item label="24" prop="price24">
+          <el-input v-model="formData.price24" placeholder="请输入第24小时的价格" type="number"/>
+        </el-form-item>
+      </div>
     </el-form>
     <template #footer>
       <el-button @click="submitForm" type="primary" :disabled="formLoading">确 定</el-button>
@@ -103,6 +105,15 @@
     </template>
   </Dialog>
 </template>
+<style scoped>
+.price-form{
+  display: flex;
+  flex-wrap:wrap ;
+  >div{
+    width: 50%;
+  }
+}
+</style>
 <script setup lang="ts">
 import {HourlyPricesApi, HourlyPricesVO} from '@/api/iot/hourlyprices'
 import {ProjectInfoApi, ProjectInfoVO} from "@/api/iot/projectinfo";
