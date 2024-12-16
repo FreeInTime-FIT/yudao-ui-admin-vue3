1e51a931ec1ffabfcf39a61572fed84de3c309cd
zhaotianzeng <tianzneg@gmail.com>
2024-12-16 15:01:21 +0800

    warning

--- src/views/screen/data/realtime.vue
+++ src/views/screen/data/realtime.vue
@@ -164,7 +164,7 @@ const getData = async () => {
     projectId: projectStore.projectInfo?.id,
   })
   keyValue.value = res.data || {};
-  warningData.value = await c({
+  warningData.value = await page({
     projectId: projectStore.projectInfo?.id,
     pageNo: '1',
     pageSize: '10'
